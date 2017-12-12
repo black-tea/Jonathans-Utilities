98f17b14711b31ddd3e2f368542f133d1aec4aa6
black-tea <timothy.c.black@gmail.com>
2017-12-12 00:02:48 -0800

    updated

--- TPS/scrape.R
+++ TPS/scrape.R
@@ -6,9 +6,15 @@ pg <- read_html('C:/Users/Tim/Documents/GitHub/Jonathans-Utilities/TPS/RA62/myma
 
 pg %>% 
   html_nodes('script') %>%
+  # Extract second 'script' node
   extract2(2) %>% 
-  html_text %>% 
+  html_text %>%
+  # Regex match formula
   str_match_all('title: "(.*)"') %>%
-  extract2(1) -> lat_lng 
+  # Pull out content within ()
+  extract2(1) -> lat_lng
 
-lat_lng
+# Grab second column, split
+lat_lng <- strsplit(lat_lng[,2], ", ")
+
+print(df)

230df994a15a68d7cb75a6d79b5b3c558463c296
black-tea <timothy.c.black@gmail.com>
2017-12-16 00:41:29 -0800

    configured df & sf df transformation

--- TPS/scrape.R
+++ TPS/scrape.R
@@ -1,10 +1,13 @@
 require(rvest)
 library(magrittr)
 library(stringr)
+library(sf)
 
-pg <- read_html('C:/Users/Tim/Documents/GitHub/Jonathans-Utilities/TPS/RA62/mymap0.html')
+# Extract html data
+html_data <- read_html('C:/Users/Tim/Documents/GitHub/Jonathans-Utilities/TPS/RA62/mymap0.html')
 
-pg %>% 
+# Process html data
+html_data %>% 
   html_nodes('script') %>%
   # Extract second 'script' node
   extract2(2) %>% 
@@ -16,5 +19,17 @@ pg %>%
 
 # Grab second column, split
 lat_lng <- strsplit(lat_lng[,2], ", ")
+
+# Convert to df, time
+df <- as.data.frame(do.call(rbind, lapply(lat_lng, rbind)))
+colnames(df) <- c("Lat", "Lng", "Timestamp")
+df$Timestamp <- strptime(df$Timestamp, format = "%d-%b-%y %H.%M.%S", "UTC")
+df$Lat <- as.numeric(as.character(df$Lat))
+df$Lng <- as.numeric(as.character(df$Lng))
+
+# Convert to sf object
+sf_df <- st_as_sf(df,
+                  coords=c("Lat","Lng"),
+                  remove=FALSE)
 
 print(df)

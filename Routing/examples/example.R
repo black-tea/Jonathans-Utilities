5e9c7a05351f6c6f25f5b5d6694b094b6d1ba371
black-tea <timothy.c.black@gmail.com>
2018-03-24 19:55:21 -0700

    routing algorithm updates

--- Routing/examples/example.R
+++ Routing/examples/example.R
@@ -3,11 +3,68 @@ library(stplanr)
 # Example source
 # https://github.com/ropensci/stplanr/blob/master/R/SpatialLinesNetwork.R
 # See lines 458 - 466
+# @section Details:
+# Find the shortest path on the network between specified nodes and returns
+# a SpatialLinesdataFrame containing the path(s) and summary statistics of
+# each one.
+#
+# @param sln The SpatialLinesNetwork to use.
+# @param start Node ID(s) of route starts.
+# @param end Node ID(s) of route ends.
+# @param sumvars Character vector of variables for which to calculate
+# summary statistics.
+# @param combinations Boolean value indicating if all combinations of start
+# and ends should be calculated. If TRUE then every start Node ID will be routed
+# to every end Node ID. This is faster than passing every combination to start
+# and end. Default is FALSE.
+#
+# @examples
+# data(routes_fast)
+# rnet <- overline(routes_fast, attrib = "length")
+# SLN <- SpatialLinesNetwork(rnet)
+# weightfield(SLN) # field used to determine shortest path
+# shortpath <- sum_network_routes(SLN, 1, 50, sumvars = "length")
+# plot(shortpath, col = "red", lwd = 4)
+# plot(SLN, add = TRUE)
+# @export
 
-data(routes_fast)
-rnet <- overline(routes_fast, attrib = "length")
-SLN <- SpatialLinesNetwork(rnet)
-weightfield(SLN) # field used to determine shortest path
-shortpath <- sum_network_routes(SLN, 1, 17, sumvars = "length")
-plot(shortpath, col = "red", lwd = 4)
-plot(SLN, add = TRUE)
+#data(routes_fast)
+#rnet <- overline(routes_fast, attrib = "length")
+#SLN <- SpatialLinesNetwork(rnet)
+#weightfield(SLN) # field used to determine shortest path
+#shortpath <- sum_network_routes(SLN, 1, 17, sumvars = "length")
+#plot(shortpath, col = "red", lwd = 4)
+#plot(SLN, add = TRUE)
+
+# Example source for sum_network_links function
+# Summarise links from shortest paths data
+# See lines 622-648
+# @section Details:
+# Find the shortest p-ath on the network between specified nodes and returns
+# a SpatialLinesDataFrame or sf containing the path(s) and summary statistics
+# of each one.
+#
+# @param sln The SpatialLinesNetwork or sfNetwork to use.
+# @param routedata A dataframe where the first column contains the Node ID(s)
+# of the start of the routes, the second column indicates the Node ID(s) of
+# the end of the routes, and any additional columns are summarised by link.
+# If there are no additional colums, then overlapping routes are counted.
+# @examples
+# data(routes_fast)
+# rnet <- overline(routes_fast, attrib = "length")
+# SLN <- SpatialLinesNetwork(rnet)
+# weightfield(SLN) # field used to determine shortest path
+# shortpath <- sum_network_links(
+#     SLN,
+#     data.frame(
+#         start=rep(c(1,2,3,4,5),each=4),
+#         end=rep(c(50,51,52,33),times=5)
+#     )
+# )
+# plot(shortpath, lwd=shortpath$count)
+#
+# @export
+# https://github.com/ropensci/stplanr/blob/master/R/SpatialLinesNetwork.R
+# See lines 458 - 466
+SLN_sf <- SpatialLinesNetwork(route_network_sf)
+plot(SLN_sf)
--- Routing/la_city_network/la_routing_test.R
+++ Routing/la_city_network/la_routing_test.R
@@ -2,11 +2,12 @@
 library(stplanr)
 library(sf)
 library(dplyr)
+library(sp)
 
 # For example of routing in R, see 
 # https://www.rdocumentation.org/packages/stplanr/versions/0.2.2/topics/SpatialLinesNetwork
 
-# Import street centerline file & format
+# Import street centerline file into sf dataframe object
 la_streets <- st_read("C:/Users/Tim/Downloads/Streets_Centerline/Streets_Centerline.shp",quiet=TRUE)
 la_streets <- la_streets %>%
   dplyr::select(ASSETID, STNUM, ID, STNAME, OLD_STREET, geometry) %>%
@@ -16,15 +17,38 @@ la_streets <- la_streets %>%
                            'Collector Street',
                            'Modified Collector Street',
                            'Major Highway - Class II',
-                           'Modified Secondary Highway'))
+                           'Modified Secondary Highway')) %>%
+  # coerce to linestring object from multi-linestring
+  st_cast('LINESTRING')
 
-# Convert to SpatialLines Network class
+# Method 1: Convert to Spatial* Object, then SpatialLinesNetwork Class
+# Convert to SpatialLinesDataFrame
+# 'as' method to coerce simple feature geometries to corresponding Spatial* objects (sp SpatialLinesDataFrame)
 la_streets_SL <- as(la_streets, "Spatial")
-la_streets_SLN <- SpatialLinesNetwork(la_streets_SL)
+# Convert to SpatialLines Network class 
+# @nb = node values
+la_streets_SLN <- SpatialLinesNetwork(la_streets_SL) 
+# Identify nodes, correspond to SpatialLinesNetwork@nb values
+la_streets_nodes <- sln2points(la_streets_SLN)
 
-# Calculate shortest path, based on distance
-shortpath <- sum_network_routes(la_streets_SLN, 300, 900, sumvars = "length")
+# Method 2: Convert to sfNetwork object (pure sf)
+# As of 3/24/18, package doesn't appear to have support for sfNetwork class
+# Should be able to take sf object, but cannot
+#la_streets_sfN <- SpatialLinesNetwork(la_streets)
 
-# Plot the results
-plot(shortpath, col = "red", lwd = 4)
-plot(la_streets_SLN, add = TRUE)
+
+# Calculate shortest path, based on distance, using node IDs
+shortpath <- sum_network_routes(la_streets_SLN, 300, 405, sumvars = "length")
+
+# Plot the results, confirming that shortpath uses node IDs (not segment IDs) for routing
+plot(shortpath, col = "red", lwd = 3)
+plot(la_streets_SLN, col = "grey", add = TRUE)
+plot(shortpath, col = "red", lwd = 3, add = TRUE)
+plot(la_streets_nodes[c(300,405),], col = "black", pch = 16, cex = 1.2, asp = 1, add=TRUE)
+
+# NEXT STEPS
+# For each node ID in la_streets_SLN@nb, get object IDs of segments
+# Join IDs to original data source, get list of unique names
+# Concatenate list of unique street names to get an intersection name, eg 'Wilshire & Vermont'
+# User searches, autocompletes intersection name to https://stackoverflow.com/questions/35265920/auto-complete-and-selection-of-multiple-values-in-text-box-shiny
+# sum_network_routes runction converts to spatial object, then show on top of leaflet library.

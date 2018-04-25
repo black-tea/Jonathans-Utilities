36eb977f5d0113817e59c363b697ab6cf43a04d5
Tim Black <timothy.c.black@gmail.com>
2018-04-25 09:28:33 -0700

    added additional map filter for LAFD points
    Previously, only used incident no. & veh_id for the map filter. To
    protect against instances where LAFD does not have unique incident
    numbers, we will also use the 3 min lag filter for drawing points /
    lines on the map

--- TPS/server.R
+++ TPS/server.R
@@ -222,7 +222,7 @@ server <- function(input, output) {
     return(tps_runs)
   })
   
-  # Reactive expression to process html files to list of sf dfs
+  # Reactive expression to process files to list of sf dfs
   lafd_points <- reactive({
     
     if(!is.null(input$lafd_files)) {
@@ -489,12 +489,13 @@ server <- function(input, output) {
       # LAFD Points
       lafd_incident <- as.numeric(match_tbl()[row_num, 7])
       lafd_veh <- as.character(match_tbl()[row_num,8])
+      lafd_run <- as.numeric(match_tbl()[row_num,6])
       lafd_points_s <- lafd_points() %>%
-        filter(incident_id == lafd_incident & veh_id == lafd_veh)
+        filter(incident_id == lafd_incident & veh_id == lafd_veh & run_id == lafd_run)
       
       # LAFD Paths
       lafd_paths_s <- lafd_paths() %>%
-        filter(incident_id == lafd_incident & veh_id == lafd_veh)
+        filter(incident_id == lafd_incident & veh_id == lafd_veh & run_id == lafd_run)
       
       # Create list of data
       match_pts_s <- list(
@@ -530,8 +531,9 @@ server <- function(input, output) {
       if(!is.null(join_tbl()[row_num, 7])){
         lafd_incident <- as.numeric(join_tbl()[row_num, 7])
         lafd_veh <- as.character(join_tbl()[row_num,8])
+        lafd_run <- as.numeric(match_tbl()[row_num,6])
         lafd_points_s <- lafd_points() %>%
-          filter(incident_id == lafd_incident & veh_id == lafd_veh)
+          filter(incident_id == lafd_incident & veh_id == lafd_veh & run_id == lafd_run)
       } else {
         lafd_points_s <- NULL
       }
@@ -539,7 +541,7 @@ server <- function(input, output) {
       # LAFD Paths
       if(!is.null(join_tbl()[row_num, 7])){
         lafd_paths_s <- lafd_paths() %>%
-          filter(incident_id == lafd_incident & veh_id == lafd_veh)
+          filter(incident_id == lafd_incident & veh_id == lafd_veh & run_id == lafd_run)
       } else {
         lafd_paths_s <- NULL
       }
